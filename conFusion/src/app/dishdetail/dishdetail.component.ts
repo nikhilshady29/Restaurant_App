4613180719c082f49ba2b6cd2ee06c8a39f0045a
nikhilshady29 <nikhilshady29@gmail.com>
2021-11-19 22:45:22 +0530

    SPAA

--- conFusion/src/app/dishdetail/dishdetail.component.ts
+++ conFusion/src/app/dishdetail/dishdetail.component.ts
@@ -3,6 +3,10 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Comment } from '../shared/comment';
 import { Dish } from '../shared/dish';
 
+import { Params, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { DishService } from '../services/dish.service';
+
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
@@ -10,14 +14,24 @@ import { Dish } from '../shared/dish';
 })
 export class DishdetailComponent implements OnInit {
 
-  @Input()
+  //@Input()
   dish: Dish;
 
   comments: Comment[];
 
-  constructor() { }
+  constructor(
+    private dishservice: DishService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) { }
 
   ngOnInit(): void {
+    const id = +this.route.snapshot.params['id'];
+    this.dish = this.dishservice.getDish(id.toString());
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
